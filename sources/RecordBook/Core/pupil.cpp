a5a9cea2919114751946c8a4df0c1dd77774a5a2
BulgakovaMargarita <bulgakova1997@mail.ru>
2016-04-11 12:23:37 +0300

    добавлен запароленный вход

--- sources/RecordBook/Core/pupil.cpp
+++ sources/RecordBook/Core/pupil.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
@@ -14,18 +15,37 @@ void Pupil::marks()
 {
    char PupilSurname[20];
    char PupilName[20];
+   int password;
+   int PassChek;
+
    cout<<"Введите фамилию и имя ученика"<<endl;
    cin>>PupilSurname>>PupilName;
+
+   cout<<"Введите пароль"<<endl;
+   cin>>password;
+
    ifstream ProgressBook;
-   ProgressBook.open("pupil.txt");
+   ProgressBook.open("/home/user/Students_Record_Book/sources/RecordBook/pupil.txt");
+
    int i;
    char s1[20];
    char s2[20];
+
    for (i=1; i<16; i++){
-       ProgressBook>>s1>>s2>>mark[russian]>>mark[literature]>>mark[algebra]>>mark[geometry]>>mark[history]>>mark[SocialStudies]>>mark[LSF]>>mark[PC]>>mark[biology]>>mark[geography];
-       if ((s1==PupilSurname) and(s2==PupilName)){
-         cout<<mark[russian]<<mark[literature]<<mark[algebra]<<mark[geometry]<<mark[history]<<mark[SocialStudies]<<mark[LSF]<<mark[PC]<<mark[biology]<<mark[geography]<<endl;
+       ProgressBook>>s1>>s2>>PassChek>>mark[russian]>>mark[literature]>>mark[algebra]>>mark[geometry]>>mark[history]>>mark[SocialStudies]>>mark[LSF]>>mark[PC]>>mark[biology]>>mark[geography];
+       if ((strcmp(s1,PupilSurname)==0) and(strcmp(s2,PupilName)==0) and (password==PassChek)){
+         cout<<"Russian "<<mark[russian]<<endl;
+         cout<<"Literature "<<mark[literature]<<endl;
+         cout<<"Algebra "<<mark[algebra]<<endl;
+         cout<<"Geometry "<<mark[geometry]<<endl;
+         cout<<"Social Studies "<<mark[SocialStudies]<<endl;
+         cout<<"Life Safety Fundamentals "<<mark[LSF]<<endl;
+         cout<<"Physical Culture "<<mark[PC]<<endl;
+         cout<<"biology "<<mark[biology]<<endl;
+         cout<<"geography "<<mark[geography]<<endl;
+         i=17;
        }
+
    }
    ProgressBook.close();
    delete []s1;
